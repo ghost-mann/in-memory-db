4a894f68a0d4eaf72d68b793ca92dc161f325a46
ghost-mann <iamaob8@protonmail.com>
2025-10-01 13:13:24 +0300

    feat: manipulating nested dictionaries in main.py

--- main.py
+++ main.py
@@ -74,5 +74,64 @@ for x in operator.values():
 for x,y in operator.items():
     print(x,y)
     
-# 
+# copying dictionaries 
+main_operator = operator.copy()
+print(f"This is{main_operator}")
+
+
+side_operator = dict(main_operator)
+print(side_operator)
+
+
+# nested dictionaries
+
+my_students = {
+    "student1": {
+        "name":"Julius",
+        "school": "infill",
+        "grade": 8
+    },
+    "student2": {
+        "name": "Silvia",
+        "school": "Newlight",
+        "grade":5
+    },
+    "student3": {
+        "name":"Kinderman",
+        "school": "Tendercare",
+        "grade": 6
+    }
+}
+
+print(my_students["student1"])
+
+# add all dictionaries to a new dictionary 
+
+kid1 = {
+  "name" : "Emil",
+  "year" : 2004
+}
+kid2 = {
+  "name" : "Tobias",
+  "year" : 2007
+}
+kid3 = {
+  "name" : "Linus",
+  "year" : 2011
+}
+
+my_school = {
+    "kid1":kid1,
+    "kid2":kid2,
+    "kid3":kid3
+}
+
+print(my_school["kid1"]["name"])
+
+# accessing items in nested dictionaries 
+
+for x, obj in my_school.items():
+    print(x)
     
+for y in obj:
+    print(y + ':', obj[y])
