df1507548fcf15a4a8a12f34efeef8186488b0a8
ghost-mann <iamaob8@protonmail.com>
2025-10-01 11:44:33 +0300

    feat: adding, changing and removing items from dictionary in main.py

--- main.py
+++ main.py
@@ -17,9 +17,43 @@ print(len(my_dict))
 print(len(my_car))
 print(len(my_ride))
 
+# Accessing items
 
 x = my_car["Model"]
 y = my_car.get("Model")
 print(x)
 print(y)
 
+z = my_ride.keys()
+print(z)
+
+# changing items
+
+my_car["Year"] = 2018
+print(my_car)
+
+my_car.update({"Year": 2020})
+print(my_car)
+
+# add items
+my_car.update({"Petrol":True})
+print(my_car)
+
+# removing items 
+my_car.pop("Diesel")
+print(my_car)
+
+my_car.popitem()
+print(my_car)
+
+# adding items
+my_car.update({"colour":"Grey"})
+my_car["rims"] = "Black" 
+print(my_car)
+
+# # empties the dictionary
+# my_car.clear()
+# print(my_car)
+
+del my_car["rims"]
+print(my_car)
